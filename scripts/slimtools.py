8e4dac500b19dc58f0e1b0fbd991eb2c5f07705c
CJ <cj@Christophers-MacBook-Pro.local>
2019-02-18 15:07:59 -0800

    slimtools updates

--- scripts/slimtools.py
+++ scripts/slimtools.py
@@ -136,7 +136,7 @@ def sample_treeseq(infile,
     sampling - 'random' (nSamples random individuals);
                'midpoint' (nSamples closest individuals to the middle of the landscape);
                'point' (nSamples/len(sampling_locs) individuals closest to each location in sampling_locs).
-    recapitate - boolean. Should simulations be run to coalescence with msprime? False unless you know *exactly* what you're doing hereself.
+    recapitate - boolean. Should simulations be run to coalescence with msprime? False unless you know *exactly* what you're doing here.
     recombination_rate - float. Recombination rate to use for recapitation, in recombination events per base per generation.
     write_to_file - boolean.
     outfile - string. File path for output.
@@ -150,19 +150,21 @@ def sample_treeseq(infile,
         ts=infile
     if(not seed==0):
         np.random.seed(seed)
+    if(recapitate):
+        ts=ts.recapitate(recombination_rate=recombination_rate) #moved from bottom bc simplify converts to msprime treeseq(??? - ask Peter)
     if(sampling=="random"):
         sample_inds=np.unique([ts.node(j).individual for j in ts.samples()]) #final-generation individuals
         subsample=np.random.choice(sample_inds,nSamples,replace=False) #get nSamples random sample inds
         subsample_nodes=[ts.individual(x).nodes for x in subsample] #node numbers for sampled individuals
         subsample_nodes=[a for b in subsample_nodes for a in b] #flatten the list
-        subsample_nodes=np.sort(np.array(subsample_nodes)) #note this is needed to preserve individual order relative to locations - should build better individual ID verifications later...
+        subsample_nodes=np.sort(np.array(subsample_nodes))
         o=ts.simplify(subsample_nodes)
         locs=np.array([[x.location[0],x.location[1]] for x in ts.individuals()])
         olocs=np.array([[x.location[0],x.location[1]] for x in o.individuals()])
         if plot:
             plt.scatter(locs[:,0], locs[:,1],s=80,facecolors='none',edgecolors='k')
             plt.scatter(olocs[:,0],olocs[:,1],s=80,facecolors='r',edgecolors='r')
-            plt.axis([0, 35, 0, 35])
+            plt.axis([0, np.max(locs[:,0]), 0, np.max(locs[:,1])])
     if(sampling=="midpoint"):
         #get final generation individuals
         extant_inds=np.unique([ts.node(j).individual for j in ts.samples()]) #final-generation individuals
@@ -188,7 +190,7 @@ def sample_treeseq(infile,
         if plot:
             plt.scatter(locs[:,0],locs[:,1],s=80,facecolors='none',edgecolors='k')
             plt.scatter(olocs[:,0],olocs[:,1],s=80,facecolors='r',edgecolors='r')
-            plt.axis([0, 35, 0, 35])
+            plt.axis([0, np.max(locs[:,0]), 0, np.max(locs[:,1])])
     if(sampling=="point"):
         #get final generation individuals
         extant_inds=np.unique([ts.node(j).individual for j in ts.samples()]) #final-generation individuals
@@ -218,34 +220,10 @@ def sample_treeseq(infile,
         if plot:
             plt.scatter(locs[:,0], locs[:,1],s=80,facecolors='none',edgecolors='k')
             plt.scatter(olocs[:,0],olocs[:,1],s=80,facecolors='r',edgecolors='r')
-            plt.axis([0, 35, 0, 35])
-    if(recapitate):
-        o=o.recapitate(recombination_rate=recombination_rate)
+            plt.axis([0, np.max(locs[:,0]), 0, np.max(locs[:,1])])
     if(write_to_file):
         o.dump(outfile)
     return o
-
-def mutate_treeseq_directory(indir,outdir,mu,seed):
-    '''
-    Add mutations at constant rate "mu" to all tree sequences in directory "indir"
-    '''
-    trees=[f for f in os.listdir(indir) if not f.startswith(".")]
-    for i in tqdm(trees):
-        ts=pyslim.load(os.path.join(indir,i))
-        ts=msp.mutate(ts,mu,random_seed=seed)
-        ts.dump(os.path.join(outdir,i))
-
-    return None
-
-def mutate_treeseq(infile,outfile,mu,seed):
-    '''
-    Add mutations at constant rate "mu" to a single tree sequence
-    '''
-    ts=pyslim.load(os.path.join(infile,i))
-    ts=msp.mutate(ts,mu,random_seed=seed)
-    ts.dump(os.path.join(outfile,i))
-
-    return None
 
 def get_ms_outs_directory(direc,subset,start=0,stop=0):
     '''
@@ -367,8 +345,10 @@ def getSLiMSumStats(haps,positions,label,locs,outfile,maxlen,ibs_tracts=True,ver
         print("calculating pairwise statistics")
     gen_dist=allel.pairwise_dxy(pos=positions,
                                 gac=genotype_allele_counts,
-                                start=0,stop=maxlen)
+                                start=0,
+                                stop=maxlen)
     sp_dist=np.array(scipy.spatial.distance.pdist(locs))
+    #sp_dist=[x+0.1 for x in sp_dist if x==0] #add 0.1 to avoid 0 distances for intra-population comparisons when taking logs (? better way to deal with this ?)
     gen_sp_corr=np.corrcoef(gen_dist,np.log(sp_dist))[0,1]
     gen_dist_skew=scipy.stats.skew(gen_dist)
     gen_dist_var=np.var(gen_dist)
@@ -409,6 +389,7 @@ def getSLiMSumStats(haps,positions,label,locs,outfile,maxlen,ibs_tracts=True,ver
 
         #summaries of the full distribution
         ibs_blocks_over_1e6=np.sum(ibs_blocks_over_1e6_pair)
+        ibs_blocks_over_1e6_per_pair=np.mean(ibs_blocks_over_1e6_pair)
         ibs_blocks_per_pair=np.mean(ibs_blocks_pair)
         ibs_flat=np.array([x for y in ibs for x in y])
         ibs_mean=np.mean(ibs_flat)
@@ -420,10 +401,10 @@ def getSLiMSumStats(haps,positions,label,locs,outfile,maxlen,ibs_tracts=True,ver
             sfsnames=["sfs_"+str(x)+" " for x in range(np.shape(haps)[1])]
             sfsnames.append("sfs_"+str(np.shape(haps)[1]))
             out=open(outfile,"w")
-            out.write("sigma segsites pi thetaW tajD het_o fis gen_dist_mean gen_dist_var gen_dist_skew gen_sp_corr ibs_mean ibs_var ibs_skew ibs_95p ibs_blocks_per_pair ibs_mean_spat_corr ibs_1e6blocks_spat_corr ibs_skew_spat_corr ibs_blocks_spat_corr"+
+            out.write("sigma segsites pi thetaW tajD het_o fis gen_dist_mean gen_dist_var gen_dist_skew gen_sp_corr ibs_mean ibs_var ibs_skew ibs_blocks_per_pair ibs_blocks_over_1e6_per_pair ibs_mean_spat_corr ibs_1e6blocks_spat_corr ibs_skew_spat_corr ibs_blocks_spat_corr "+
             "".join(sfsnames)+"\n")
         row=[label,segsites,pi,thetaW,tajD,het_o,fis,gen_dist_mean,gen_dist_var,gen_dist_skew,gen_sp_corr,
-             ibs_mean,ibs_var,ibs_skew,ibs_blocks_per_pair,ibs_mean_spat_corr,ibs_1e6blocks_spat_corr,ibs_skew_spat_corr,
+             ibs_mean,ibs_var,ibs_skew,ibs_blocks_per_pair,ibs_blocks_over_1e6_per_pair,ibs_mean_spat_corr,ibs_1e6blocks_spat_corr,ibs_skew_spat_corr,
              ibs_blocks_spat_corr]
         row=np.append(row,sfs)
         row=" ".join(map(str, row))
@@ -443,223 +424,3 @@ def getSLiMSumStats(haps,positions,label,locs,outfile,maxlen,ibs_tracts=True,ver
     out.close()
 
     return(out)
-
-def getSLiMSumStats_directory(haps,positions,labels,locs,outfile,maxlen,ibs_tracts=True,verbose=True):
-    for i in tqdm(range(len(haps))):
-        if(verbose):
-            print("processing simulation "+str(i))
-
-        #load in genotypes etc into scikit-allel
-        genotypes=allel.HaplotypeArray(haps[i]).to_genotypes(ploidy=2)
-        allele_counts=genotypes.count_alleles()
-        genotype_allele_counts=genotypes.to_allele_counts()
-
-        if(verbose):
-             print("calculating sample-wide statistics")
-        #nonspatial population-wide summary statistics
-        segsites=np.shape(genotypes)[0]
-        mpd=np.mean(allel.diversity.mean_pairwise_difference(allele_counts))
-        pi=(mpd*segsites)/maxlen
-        tajD=allel.diversity.tajima_d(ac=allele_counts,start=1,stop=maxlen)
-        thetaW=allel.diversity.watterson_theta(pos=positions[i],ac=allele_counts,start=1,stop=maxlen)
-        het_o=np.mean(allel.heterozygosity_observed(genotypes))
-        fis=np.mean(allel.inbreeding_coefficient(genotypes))
-        sfs=allel.sfs(allele_counts[:,1]) #last entry seems to be the highest *non-zero* SFS entry (wtf?) so adding zeros
-        sfs=np.append(sfs,[np.repeat(0,np.shape(haps[i])[1]-len(sfs)+1)])
-
-        #Isolation by distance
-        if(verbose):
-            print("calculating pairwise statistics")
-        gen_dist=allel.pairwise_dxy(pos=positions[i],
-                                    gac=genotype_allele_counts,
-                                    start=0,stop=maxlen) #SLOOOOOW - issue with noninteger positions?
-        sp_dist=np.array(scipy.spatial.distance.pdist(locs[i]))
-
-        #IBS tract length summaries
-        if(verbose):
-            print("calculating IBS tract lengths")
-        if(ibs_tracts):
-            pairs=itertools.combinations(range(np.shape(haps[i])[1]),2)
-            ibs=[]
-            spat_dists=[]
-            locs2=np.repeat(locs[i],2,0)
-            for j in pairs:
-                ibs.append(getPairwiseIbsTractLengths(haps[i][:,j[0]],
-                                                      haps[i][:,j[1]],
-                                                      positions[i],
-                                                      1e8))
-                spat_dists.append(scipy.spatial.distance.euclidean(locs2[j[0]],locs2[j[1]]))
-
-            ibs_flat=[x for y in ibs for x in y]
-            ibs_95p=np.percentile(ibs_flat,95)
-            ibs_mean=np.mean(ibs_flat)
-            ibs_var=np.var(ibs_flat)
-            ibs_skew=scipy.stats.skew(ibs_flat)
-            ibs_mean_spat_corr=np.corrcoef([np.mean(x) for x in ibs],spat_dists)[0,1]
-
-            #ibs_over_1e6=len([x for x in ibs_flat if x>=1e6]) #way too slow
-            ibs_num_blocks_per_pair=len(ibs_flat)/scipy.special.comb(np.shape(haps[i])[1],2)
-            #ibs_binned=scipy.stats.binned_statistic(x=ibs_flat,
-            #                                        values=ibs_flat,
-            #                                        statistic='count',
-            #                                        bins=np.arange(0,1e8,2e4))
-            #np.savetxt("/Users/cj/Desktop/ibs_test.txt",ibs_flat)
-
-        #summaries of pairwise stats as a function of geographic distance
-        gen_sp_corr=np.corrcoef(gen_dist,sp_dist)[0,1]
-
-        #row=np.append(sp_dist,gen_dist,sfs)
-        if(ibs_tracts):
-            row=[labels[i],segsites,pi,thetaW,tajD,het_o,fis,gen_sp_corr,
-                 ibs_mean,ibs_var,ibs_skew,ibs_95p,ibs_num_blocks_per_pair,ibs_mean_spat_corr]
-            row=np.append(row,sfs)
-            #row=np.append(row,ibs_binned[0])
-        else:
-            row=[labels[i],segsites,pi,thetaW,tajD,het_o,fis,gen_sp_corr]
-            row=np.append(row,sfs)
-
-        if(i==0):
-            out=np.zeros(shape=(len(haps),len(row)))
-            out[i]=row
-        else:
-            out[i]=row
-        if(outfile):
-            np.savetxt(outfile,out)
-
-    return(out)
-################ example pipeline use ####################
-
-#check treeseq directory for coalescence
-# treeseqs,out=check_treeseq_coalescence("/Users/cj/spaceness/sims/slimout/spatial/W16")
-# np.mean(out[0]) #mean proportion uncoalesced gene trees
-# np.mean(out[1]) #median roots per gene tree
-#
-# sample_treeseq_directory(indir="/Users/cj/spaceness/sims/slimout/spatial/W16",
-#                          outdir="/Users/cj/spaceness/sims/sampled/spatial/W16",
-#                          nSamples=50,
-#                          recapitate=True,
-#                          recombination_rate=1e-9,
-#                          output_pop_sizes=True,
-#                          pop_size_outpath="/Users/cj/spaceness/sumstats/popsize_W16_rm.txt")
-#
-# mutate_treeseqs("/Users/cj/spaceness/sims/sampled/spatial/W16/",
-#                 "/Users/cj/spaceness/sims/mutated/spatial/W16/",
-#                 1e-8)
-#
-#haps,positions,labels,locs=get_ms_outs("/Users/cj/spaceness/sims/mutated/spatial/W16",
-#                                        False)
-#
-#positions=discretize_snp_positions(positions)
-#
-# ss=getHaplotypeSumStats(haps,positions,labels,locs,
-#                         outfile="/Users/cj/spaceness/sumstats/ss_spatial_W16.txt",
-#                         verbose=False)
-
-
-
-
-
-# #output proportion uncoalesced & dispersal for 10k outs
-# np.savetxt("/Users/cj/spaceness/uncoal_prop.txt",np.transpose(out))
-# treeseqs=[f for f in os.listdir("/Users/cj/spaceness/sims/slimout/10kouts/") if not f.startswith(".")]
-# tmp=open("/Users/cj/spaceness/uncoal_tsnames.txt","w")
-# for i in range(len(treeseqs)):
-#     tmp.write(treeseqs[i]+"\n")
-# tmp.close()
-#
-# #tree names to get generations for 10k outs
-# treeseqs=os.listdir("/Users/cj/spaceness/sims/10kouts")
-# o=open("/Users/cj/spaceness/10kout_treenames.txt")
-# for i in range(len(treeseqs)):
-#     o.write(treeseqs[i]+"\n")
-# o.close()
-
-###################################################
-#
-# def sample_treeseq(infile,outfile,nSamples,recapitate,recombination_rate,write_to_file,seed=12345):
-#     '''
-#     Sample individuals from a tree sequence then simplify and (optionally)
-#     recapitate the tree with msprime.
-#     '''
-#     if type(infile)==str:
-#         ts=pyslim.load(infile)
-#     else:
-#         ts=infile
-#     if(not seed==0):
-#         np.random.seed(seed)
-#     sample_inds=np.unique([ts.node(j).individual for j in ts.samples()]) #final-generation individuals
-#     subsample=np.random.choice(sample_inds,nSamples,replace=False) #get nSamples random sample inds
-#     subsample_nodes=[ts.individual(x).nodes for x in subsample] #node numbers for sampled individuals
-#     subsample_nodes=[a for b in subsample_nodes for a in b] #flatten the list
-#     subsample_nodes=np.sort(np.array(subsample_nodes)) #note this is needed to preserve individual order relative to locations - should build better individual ID verifications later...
-#     o=ts.simplify(subsample_nodes)
-#     if(recapitate):
-#         ts=ts.recapitate(recombination_rate=recombination_rate)
-#     if(write_to_file):
-#         o.dump(outfile)
-#
-#     return o
-
-## old pipeline using multiple presampled param values
-# def sample_sim_params(params_to_sample,
-#                       min,
-#                       max,
-#                       nreps,
-#                       outfile=False):
-#     '''
-#     Sample parameter values from uniform distributions and write to file.
-#     params_to_sample, min, and max should be arrays, even if length 1
-#     (eg min=[0.2]).
-#     '''
-#     vals=np.zeros((len(params_to_sample),nreps))
-#     for i in range(len(params_to_sample)):
-#         for j in range(nreps):
-#             val=np.random.uniform(min[i],max[i])
-#             vals[i][j]=val
-#     if(outfile):
-#         header=" ".join(params_to_sample)
-#         if(os.path.exists(outfile)):
-#             print("warning! Outfile already exists. Delete and try again.")
-#         np.savetxt(outfile,vals,header=header)
-#     return vals
-
-## old version of run_one_slim_sim that pulls multiple sampled param values from an existing file
-# def run_one_slim_sim(sim_num,
-#                      sampled_params,
-#                      slim_path,
-#                      slim_recipe,
-#                      outdir):
-#     '''
-#     Run one SLiM simulation pulling parameter values from the text file
-#     at path "sampled_params" (ie the output of sample_sim_params).
-#     Output file names are the index (row) of the sampled parameter files.
-#     '''
-#
-#     #get sampled param names and values
-#     with open(sampled_params) as f:
-#         params_to_sample = f.readline()
-#     params_to_sample=re.sub("\n|# ","",params_to_sample)
-#     params_to_sample=params_to_sample.split(" ")
-#     vals=np.loadtxt(sampled_params)
-#
-#     #get output file path
-#     filename = str(sim_num) + ".trees"
-#     filepath = os.path.join(outdir,filename)
-#
-#     #set strings for defining SLiM variables
-#     label_strs=[params_to_sample[i]+"="+str(vals[i][sim_num])
-#                 for i in range(len(params_to_sample))]
-#     output_str = "outpath='"+str(filepath)+"'"
-#
-#     #format params for subprocess.check_output
-#     command=[slim_path,
-#              "-d",output_str]
-#     for i in range(len(params_to_sample)):
-#         command.append("-d")
-#         command.append(label_strs[i])
-#     command.append(slim_recipe)
-#
-#     #run it
-#     subprocess.check_output(command)
-#
-#     return None
